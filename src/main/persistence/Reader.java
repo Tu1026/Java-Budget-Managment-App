64beeb1cfd334ed6ee0e291346336dabad958c46
linshuan <$Chubbyt566>
2020-02-23 21:16:30 -0800

    quick

--- src/main/persistence/Reader.java
+++ src/main/persistence/Reader.java
@@ -1,4 +1,6 @@
 package persistence;
 
+// Reference code from the tellerApp
+//A reader reads different data from a file
 public class Reader {
 }
--- src/main/persistence/Writer.java
+++ src/main/persistence/Writer.java
@@ -1,4 +1,15 @@
 package persistence;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+// Reference code from the tellerApp
+// A writer that writes account data to a file
 public class Writer {
+    private PrintWriter printWriter;
+
+    //EFFECTS: Construct a writer that will write data to a file
+    public Writer(File file) throws FileNotFoundException, UnsupportedEncodingException
 }
